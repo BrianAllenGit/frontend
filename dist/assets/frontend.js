12c7ad882e24128dbd80e7804751721e4fd34b23
beckah <rebeccasheeler@gmail.com>
2017-04-21 10:53:02 -0400

    added mix panel. built for prod.

--- dist/assets/frontend.js
+++ dist/assets/frontend.js
@@ -2079,7 +2079,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("frontend/app")["default"].create({"name":"frontend","version":"0.0.0+135a6f03"});
+  require("frontend/app")["default"].create({"name":"frontend","version":"0.0.0+1296f722"});
 }
 
 /* jshint ignore:end */
